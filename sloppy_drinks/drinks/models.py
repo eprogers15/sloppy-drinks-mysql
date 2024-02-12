0e5659ada81774f1526b837037181d2ddb5623a3
Emmett Rogers <eprogers15@icloud.com>
2024-02-12 18:07:09 -0500

    Create RecipeSource table

--- sloppy_drinks/drinks/models.py
+++ sloppy_drinks/drinks/models.py
@@ -5,3 +5,7 @@ class Ingredient(models.Model):
     """Model representing a drink ingredient"""
     name = models.CharField(max_length=100, primary_key=True, help_text='Enter an ingredient name (e.g. Simple Syrup)')
 
+class RecipeSource(models.Model):
+    """Model representing a recipe source"""
+    name = models.CharField(max_length=100, primary_key=True)
+    url = models.URLField(max_length=200, blank=True, null=True)
