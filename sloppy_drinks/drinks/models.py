8224fd1f78c2396de9acd8b1216f37795bfb1f8a
Emmett Rogers <eprogers15@icloud.com>
2024-02-12 18:19:07 -0500

    Create Drink table

--- sloppy_drinks/drinks/models.py
+++ sloppy_drinks/drinks/models.py
@@ -10,6 +10,14 @@ class RecipeSource(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     url = models.URLField(max_length=200, blank=True, null=True)
 
+class Drink(models.Model):
+    """Model representing a drink"""
+    name = models.CharField(max_length=200, primary_key=True)
+    slug = models.SlugField(default="", null=False)
+    ingredients = models.ManyToManyField(Ingredient, help_text='Select the ingredients for this drink')
+    recipe_source = models.ForeignKey(RecipeSource, blank=True, null=True, on_delete=models.PROTECT)
+    recipe_url = models.URLField(max_length=200, blank=True, null=True)
+
 class ImageSource(models.Model):
     """Model representing an image source"""
     name = models.CharField(max_length=100, primary_key=True)
