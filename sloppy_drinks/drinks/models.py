df0e3493bed9c4214056f453789270114d6079c5
Emmett Rogers <eprogers15@icloud.com>
2024-02-12 18:00:59 -0500

    Add drinks app to settings.py and create Ingredient table

--- sloppy_drinks/drinks/models.py
+++ sloppy_drinks/drinks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    """Model representing a drink ingredient"""
+    name = models.CharField(max_length=100, primary_key=True, help_text='Enter an ingredient name (e.g. Simple Syrup)')
+
