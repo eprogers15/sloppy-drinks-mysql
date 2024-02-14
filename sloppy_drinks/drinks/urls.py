3990779a5b446ba5d1659da3931e4ffe0133d03b
Emmett Rogers <eprogers15@icloud.com>
2024-02-13 22:07:37 -0500

    Rename view and template 'home' to 'index'

--- sloppy_drinks/drinks/urls.py
+++ sloppy_drinks/drinks/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from drinks import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.index, name='index')
 ]
--- sloppy_drinks/drinks/views.py
+++ sloppy_drinks/drinks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, "drinks/home.html", {})
+def index(request):
+    return render(request, "drinks/index.html", {})
