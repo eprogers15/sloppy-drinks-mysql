5ccefe4d365f56d0316c6a075c0ed0dfa07a45fb
Emmett Rogers <eprogers15@icloud.com>
2024-02-13 22:49:13 -0500

    Configure Admin for models and add first drink Tom Collins to test

--- sloppy_drinks/drinks/admin.py
+++ sloppy_drinks/drinks/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from drinks.models import *
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+class RecipeSourceAdmin(admin.ModelAdmin):
+    pass
+
+class DrinkAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+class EpisodeAdmin(admin.ModelAdmin):
+    pass
+
+class ImageSourceAdmin(admin.ModelAdmin):
+    pass
+
+class ImageAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(RecipeSource, RecipeSourceAdmin)
+admin.site.register(Drink, DrinkAdmin)
+admin.site.register(Episode, EpisodeAdmin)
+admin.site.register(ImageSource, ImageSourceAdmin)
+admin.site.register(Image, ImageAdmin)
