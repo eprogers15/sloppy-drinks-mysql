1d45b391cd22d0c1c96ca3452171f414fd710141
Emmett Rogers <eprogers15@icloud.com>
2024-02-15 22:07:55 -0500

    Create drink_detail.html template and media directory for static files, and add second drink Zombie

--- sloppy_drinks/drinks/urls.py
+++ sloppy_drinks/drinks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from drinks import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.drink_index, name='drink_index'),
+    path("<slug:slug>/", views.drink_detail, name="drink_detail"),
 ]
--- sloppy_drinks/drinks/views.py
+++ sloppy_drinks/drinks/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from drinks.models import Drink, Image
 
 # Create your views here.
-def index(request):
-    return render(request, "drinks/index.html", {})
+def drink_index(request):
+    images = Image.objects.all()
+
+    context = {
+        "images": images,
+    }
+    return render(request, "drinks/drink_index.html", context)
+
+def drink_detail(request, slug):
+    drink = Drink.objects.get(slug=slug)
+    context = {
+        "drink": drink
+    }
+    return render(request, "drinks/drink_detail.html", context)
