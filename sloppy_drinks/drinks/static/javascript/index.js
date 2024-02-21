19a90631cec10af38407e923ab3d19fcedb2f49f
Emmett Rogers <eprogers15@icloud.com>
2024-02-20 21:54:44 -0500

    Add Waypoints to project

--- sloppy_drinks/drinks/static/javascript/index.js
+++ sloppy_drinks/drinks/static/javascript/index.js
@@ -1,9 +1,23 @@
-let padding = $("nav").height() + 12 + "px";
-$("body").css("paddingTop", padding)
+// Set initial starting point of body content based on navbar height
+let padding = $("nav").height() + 16 + "px";
+$("body").css("paddingTop", padding);
 
+// Reset starting point of body content based on navbar height after button click
 $(".navbar-toggler").click(function() {
     setTimeout(function() {
-        let padding = $("nav").height() + 12 + "px";
+        let padding = $("nav").height() + 16 + "px";
         $("body").css("paddingTop", padding)
     }, 500)
-})
+});
+
+// Reset starting point of body content based on navbar height after window resizing
+$(window).resize(function() {
+    setTimeout(function() {
+        let padding = $("nav").height() + 16 + "px";
+        $("body").css("paddingTop", padding)
+    }, 500)
+});
+
+var infinite = new Waypoint.Infinite({
+  element: $(".infinite-container")[0],
+});
