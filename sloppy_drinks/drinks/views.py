03f6d1e9228824b45d6b566b4c27c6c5ad6a5e69
Emmett Rogers <eprogers15@icloud.com>
2024-02-24 18:52:26 -0500

    Style drink_detail.html page

--- sloppy_drinks/drinks/views.py
+++ sloppy_drinks/drinks/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from drinks.models import Drink, Image
+from drinks.models import Drink, Image, Episode
 
 # Create your views here.
 def drink_index(request):
-    images = Image.objects.all()
+    images = Image.objects.filter(recipe=True)
 
     context = {
         "images": images,
@@ -12,7 +12,11 @@ def drink_index(request):
 
 def drink_detail(request, slug):
     drink = Drink.objects.get(slug=slug)
+    images = Image.objects.filter(drink=drink)
+    episodes = Episode.objects.filter(drink=drink)
     context = {
-        "drink": drink
+        "drink": drink,
+        "images": images,
+        "episodes": episodes,
     }
     return render(request, "drink_detail.html", context)
