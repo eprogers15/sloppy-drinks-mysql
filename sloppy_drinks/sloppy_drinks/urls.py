1fab8037e533b6b4b11b550324a6e260b4f8d857
Emmett Rogers <eprogers15@icloud.com>
2024-03-13 20:47:49 -0400

    Reconfigure project to serve static files from root directory

--- sloppy_drinks/sloppy_drinks/urls.py
+++ sloppy_drinks/sloppy_drinks/urls.py
@@ -21,4 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('drinks.urls')),]
+    path('', include('drinks.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
