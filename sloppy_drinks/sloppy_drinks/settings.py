56af20851e18e6710a93d6cf4fb32ceb1a44eb97
Emmett Rogers <eprogers15@icloud.com>
2024-03-13 21:07:45 -0400

    Configure settings for public hosting

--- sloppy_drinks/sloppy_drinks/settings.py
+++ sloppy_drinks/sloppy_drinks/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sloppydrinks.com', 'www.sloppydrinks.com']
 
 
 # Application definition
@@ -128,10 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = '/home/dh_dch6fh/sloppydrinks.com/public/sloppy_drinks/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+MEDIA_ROOT = '/home/dh_dch6fh/sloppydrinks.com/public/sloppy_drinks/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
